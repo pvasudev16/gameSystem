d7c3eff51963be815dc245ab87712186bce5b929
pvasudev16 <45635456+pvasudev16@users.noreply.github.com>
2021-07-10 12:09:23 -0700

    Created header and cpp file for Processor class

---
+++ gameSystem/processor.cpp
@@ -0,0 +1,9 @@
+//
+//  processor.cpp
+//  gameSystem
+//
+//  Created by Pranai Vasudev on 2021-07-10.
+//
+
+#include <stdio.h>
+#include "processor.h"
---
+++ gameSystem/processor.h
@@ -0,0 +1,16 @@
+//
+//  processor.h
+//  gameSystem
+//
+//  Created by Pranai Vasudev on 2021-07-10.
+//
+
+#ifndef processor_h
+#define processor_h
+
+class Processor {
+public:
+    void receivePacket(int *packet, size_t packetSize);
+}
+
+#endif /* processor_h */
